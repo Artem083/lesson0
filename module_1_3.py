0f635b9842f81653620df8a6c45b7ed5b36d2d74
Artem083 <9161836219@mail.ru>
2024-08-11 12:50:21 +0300

    Add files via upload
    Доработал по комментарию

---
+++ module_1_3.py
@@ -0,0 +1,7 @@
+name = 'Artem'  # Первый вариант - но это слишком просто, для того чтоб быть верным...
+print('Name' + ':', name)
+age = 41
+print('Age' + ':', age)
+print('New Age' + ':', age + 1)
+is_student = True
+print('Is Student' ":", is_student)
