23e4e180243f17a58dd06d4d9769de6b1ef329a7
Artem083 <9161836219@mail.ru>
2024-08-11 14:12:27 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string: str = input('Привет! Как твои успехи? ')  # 2. Организуйте программу.
+print(len(my_string))
+print(my_string.upper())  # 3. Работа с методами строк.
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[:1])
+print(my_string[-1:])
