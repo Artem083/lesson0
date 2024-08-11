d230109bb200ebc90946b50ccccdf4b4cef44c1c
Artem083 <9161836219@mail.ru>
2024-08-11 08:34:37 +0300

    Update ПЗ Переменные.py

--- ПЗ Переменные.py
+++ ПЗ Переменные.py
@@ -1,5 +1,6 @@
-home_works = '12'
-work_time = '1.5'
+home_works = 12
+work_time = 1.5
 name_of_course = "Phyton"
-print("Курс" + ":", name_of_course + ', ' + "всего задач" + ":", home_works + ', '
-      + "затраченно часов" + ':', work_time + ', ' "среднее время выполнения", float(work_time) / int(home_works))
+avg_time_on_one = work_time / home_works
+print("Курс" + ":", name_of_course + ",", "всего задач" + ":", home_works, ","
+      "затраченно часов" + ":", work_time, "среднее время выполнения", avg_time_on_one)
