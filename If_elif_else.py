6540c1e3412e4ea8e8ee70cce5a2586e63fb8581
Artem083 <9161836219@mail.ru>
2024-08-23 21:53:28 +0300

    Add files via upload

---
+++ If_elif_else.py
@@ -0,0 +1,11 @@
+first = int(input('Введите целое число '))  # - Если равных чисел среди 3-х вообще нет, то вывести 0
+second = int(input('Ввведите целое число '))
+third = int(input('Введите целое число '))
+if first != second and first != third and second != third:
+    print(0)
+elif first == second and not first == third:  # - Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
+    print(2)
+elif first == third and not second == first:
+    print(2)
+else:
+    print(3)    # - Если все числа равны между собой, то вывести 3
