ab592118ee95c76fc89ced8e1333671ea4edbf7e
Artem083 <9161836219@mail.ru>
2024-08-27 15:00:26 +0300

    Add files via upload

---
+++ WHILE_break_continue.py
@@ -0,0 +1,99 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+    number = my_list[i]
+    number - my_list[i]
+    if number > 0:
+        print(number)
+    i = i + 1
+    if number < 0:
+        break
+
+
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+#
+# number = my_list[i]
+# s = my_list[i] - my_list[i]
+# print(i, number ,s)
+# i = i + 1
+
+# s = my_list[1] - my_list[1]
+# print(i,s)
+# i=i+1
+# s = my_list[2] -  my_list[2]
+# print(i,s)
+# i=i+1
+# s = my_list[3] -  my_list[3]
+# print(i,s)
+# i=i+1
+# s = my_list[4] -  my_list[4]
+# print(i,s)
+# i=i+1
+# s = my_list[5] -  my_list[5]
+# print(i,s)
+# i=i+1
+# s = my_list[6] -  my_list[6]
+# print(i,s)
+# i=i+1
+# s = my_list[7] -  my_list[7]
+# print(i,s)
+# i=i+1
+# s = my_list[8] -  my_list[8]
+# print(i,s)
+# i=i+1
+# s = my_list[9] -  my_list[9]
+# print(i,s)
+# i=i+1
+# s = my_list[10] -  my_list[10]
+# print(i,s)
+# i=i+1
+# s = my_list[11] -  my_list[11]
+# print(i,s)
+# i=i+1
+#
