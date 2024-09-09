d6aa39ee9f84e11bf34e7730150f238d4cad3ab7
Artem083 <9161836219@mail.ru>
2024-09-09 06:46:20 +0300

    Add files via upload

--- GLOBAL_NAMES.py
+++ GLOBAL_NAMES.py
@@ -1,19 +1,23 @@
-calls = 0
-def count_calls():
-    global calls
-    calls += 1
+calls = 0   # 1.Создать переменную calls = 0 вне функций.
 
-def string_info(string):
-    stroka = str(string)
-    result = (len(stroka)),stroka.upper(),stroka.lower()
-    count_calls()
+
+def count_calls():   # 2. Создать функцию count_calls и изменять в ней значение переменной calls.
+    global calls     # Эта функция должна вызываться в остальных двух функциях.
+    calls += 1       # Функция count_calls подсчитывающая вызовы остальных функций.
+
+
+def string_info(string):   # 3. Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+    stroka = str(string)     # Функция string_info принимает аргумент - строку и возвращает кортеж из:
+    result = (len(stroka)), stroka.upper(), stroka.lower()   # длины этой строки, строку в верхнем регистре,
+    count_calls()              # строку в нижнем регистре.
     return result
 
-def is_contains(string, list_to_search):
-    string = str(string).lower()
-    list_to_search = list(list_to_search)
-    count_calls()
-    for i in range(len(list_to_search)):
+
+def is_contains(string, list_to_search):  # 4. Создать функцию is_contains с двумя параметрами string и list_to_search,
+    string = str(string).lower()  # реализовать логику работы по описанию.
+    list_to_search = list(list_to_search)   # Функция is_contains принимает два аргумента: строку и список,
+    count_calls()   # и возвращает True, если строка находится в этом списке, False - если отсутствует.
+    for i in range(len(list_to_search)):  # Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
         if str(list_to_search[i]).lower() == string:
             result = True
             break
@@ -21,8 +25,10 @@ def is_contains(string, list_to_search):
             result = False
             continue
     return result
-print(string_info('capybara'))
-print(string_info('Armageddon'))
-print(is_contains('Urban',['ban','BaNaN','urBAN']))
-print(is_contains('cycle',['recycling','cyclic']))
+
+
+print(string_info('capybara'))    # Вызвать соответствующие функции string_info и is_contains
+print(string_info('Armageddon'))   # произвольное кол-во раз с произвольными данными.
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
