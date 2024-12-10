9ededa8d1f4dbf748e795a6af4a82db46123d09f
Artem083 <9161836219@mail.ru>
2024-12-10 04:26:35 +0300

    Education

--- 7_Module/7_Task_Positining_in_the_file.py
+++ 7_Module/7_Task_Positining_in_the_file.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
         'Because there are 2 languages!',
         'Спасибо!'
         ]
-    result = custom_write('test.txt', info)
+    result = custom_write('../test.txt', info)
     for elem in result.items():
         print(elem)
--- 7_Module/7_The_WITH_operator.py
+++ 7_Module/7_The_WITH_operator.py
@@ -29,7 +29,7 @@ class WordsFinder:
 
 
 
-finder2 = WordsFinder('test_file.txt')
+finder2 = WordsFinder('../test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
